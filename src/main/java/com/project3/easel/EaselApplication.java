491c32309051fd274ad36e1865c89b0ed59a75c0
charliedolton <charliedolton1@gmail.com>
2022-04-12 17:39:12 -0700

    Initial Commit

---
+++ src/main/java/com/project3/easel/EaselApplication.java
@@ -0,0 +1,13 @@
+package com.project3.easel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaselApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaselApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project3/easel/EaselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project3.easel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaselApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
